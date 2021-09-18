cf60aaacf2b4c7d9f0309a2ae52f8c5ff579fa39
minisera <koki.ohtaka0821@gmail.com>
2021-09-18 17:51:28 +0900

    idtokenをもとにuidを取得するよう実装

--- backend/app/Http/Controllers/Api/v1/SignInController.php
+++ backend/app/Http/Controllers/Api/v1/SignInController.php
@@ -2,10 +2,10 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
+use Firebase\Auth\Token\Exception\InvalidToken;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Kreait\Firebase\Auth;
-
 
 class SignInController extends Controller
 {
@@ -16,6 +16,7 @@ class SignInController extends Controller
 
     /**
      * @param Auth $auth
+     * @param Firebase $firebase
      */
     public function __construct(Auth $auth)
     {
@@ -31,9 +32,28 @@ class SignInController extends Controller
         $email = $request->input('email');
         $password = $request->input('password');
         $signInResult = $this->auth->signInWithEmailAndPassword($email, $password);
-        $id_token = $signInResult->data()['idToken'];
+        $idToken = $signInResult->data()['idToken'];
+
+        // TODO
+        // idトークンの取得と検証を別メソッドにする
+        
+        try {
+            // firebaseから渡されたidトークンを検証する
+            $verifiedIdToken = $this->auth->verifyIdToken($idToken);
+        } catch (InvalidToken $e) {
+            return response()->json([
+                'error' => 'idトークンが違います'
+            ]);
+        }
+
+        // ユーザーの固有ID uid を取得する
+        $uid = $verifiedIdToken->claims()->get('sub');
+
+        // uidをもとにユーザー情報を取得する
+        $user = $this->auth->getUser($uid);
+
         return response()->json([
-            'idToken' => $id_token
+            'user' => $user
         ]);
     }
 }
